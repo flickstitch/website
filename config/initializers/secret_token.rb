a4388c642140881ffdb3aad87be789883370d8fe
Daniel <robotright@gmail.com>
2012-01-26 17:14:37 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collabio::Application.config.secret_token = '68f893cef3b4a8d494205bb6d2ad263aa0cc17cfd6013a43a2084f432a203e782ce54ed45e6d2c5051fe774ebc7fd54ee4b58efb8118544ea23abdb64d476c1c'
