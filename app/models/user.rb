2c7b8d6f1946c3281e2d9223ae2a63806f3745a7
Daniel <robotright@gmail.com>
2012-01-28 18:38:46 +0800

    add validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+
+  validates :username, :presence => true
+  validates :username, :uniqueness => true
+  validates :username, :format => { :with => /^[a-zA-Z0-9_]{3,20}$/, 
+    :message => "must be combination of numbers and/or letters between 3 to 20 characters long" }
 end
