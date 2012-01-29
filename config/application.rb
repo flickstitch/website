4dcd482a97aeda4ac11f7e3c807ae63d4a599573
Daniel <robotright@gmail.com>
2012-01-29 15:54:22 +0800

    get precompile to work on Heroku

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Collabio
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # to get precompile working on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
