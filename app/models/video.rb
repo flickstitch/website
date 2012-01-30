c6bb0e3e09a27442388bb5a8e45a3896e9ec90b9
Daniel <robotright@gmail.com>
2012-01-31 01:55:26 +0800

    use video_id in embed video link

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,7 @@ class Video < ActiveRecord::Base
   validate :from_youtube
 
   before_save :get_video_id, :get_thumbnail_url
+
   default_scope order('id desc')
 
   private
@@ -16,9 +17,9 @@ class Video < ActiveRecord::Base
     end
   end
 
-  def get_thumbnail_url
+  def get_video_id
+    video_id = 0
     uri = URI.parse(self.video_url)
-    video_id = ''
 
     if uri.host.include?('youtube.com')
       query = CGI.parse(uri.query)
@@ -26,10 +27,23 @@ class Video < ActiveRecord::Base
     elsif uri.host.include?('youtu.be')
       video_id = uri.path[1..-1]
     else
-      raise "youtube video could not be parsed. video url=#{self.video_url}"
+      raise "cannot figure out video id for: #{self.video_url}"
     end
 
-    self.thumbnail_url = "http://img.youtube.com/vi/#{video_id}/2.jpg"
+    self.video_id = video_id
+  end
+
+  def get_thumbnail_url
+    thumbnail_url = ''
+    uri = URI.parse(self.video_url)
+
+    if uri.host.include?('youtube.com') || uri.host.include?('youtu.be')
+      thumbnail_url = "http://img.youtube.com/vi/#{self.video_id}/2.jpg"
+    else
+      raise "unable to parse thumbnail url for: #{self.video_url}"
+    end
+
+    self.thumbnail_url = thumbnail_url
   end
 
 end
