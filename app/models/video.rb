87d3ac101a8a89917d3e48249dadc85ce4581e51
Daniel <robotright@gmail.com>
2012-01-31 00:40:07 +0800

    first version of adding youtube videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,34 @@
 class Video < ActiveRecord::Base
   validates :video_url, :presence => true
+  validate :from_youtube
+
+  before_create :get_thumbnail_url
+
+  private
+
+  # videos must be from youtube.com or vimeo.com
+  def from_youtube
+    uri = URI.parse(self.video_url)
+
+    if uri.host.nil? || (!uri.host.include?('youtube.com') && !uri.host.include?('youtu.be'))
+      errors.add(:video_url, "must be from youtube.com or vimeo.com")
+    end
+  end
+
+  def get_thumbnail_url
+    uri = URI.parse(self.video_url)
+    video_id = ''
+
+    if uri.host.include?('youtube.com')
+      query = CGI.parse(uri.query)
+      video_id = query['v'][0]
+    elsif uri.host.include?('youtu.be')
+      video_id = uri.path[1..-1]
+    else
+      raise "youtube video could not be parsed. video url=#{self.video_url}"
+    end
+
+    self.thumbnail_url = "http://img.youtube.com/vi/#{video_id}/2.jpg"
+  end
+
 end
