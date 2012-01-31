1942c1e51e9626baf4017622b2435db0d0b439d3
Daniel <robotright@gmail.com>
2012-01-31 08:22:35 +0800

    get rid of factorygirl dupe load error

--- spec/factories.rb
+++
@@ -1,6 +0,0 @@
-Factory.define :user do |u|
-  u.sequence(:id) { |n| n }
-  u.sequence(:username) { |n| "person#{n}" }
-  u.sequence(:email) { |n| "person#{n}@email.com" }
-  u.password 'jjjjjj'
-end
