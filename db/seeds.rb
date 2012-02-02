aeb60bd93c225adfcd7c62ce1619fcb85cabb12f
Daniel <robotright@gmail.com>
2012-02-02 10:41:25 +0800

    add roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# roles
+Role.find_or_create_by_name(:name => :admin)
+Role.find_or_create_by_name(:name => :manager)
