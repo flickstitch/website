e649238934674d0c9b6664f6b75e729d61f7cbac
Daniel <robotright@gmail.com>
2012-02-02 09:38:45 +0800

    redirect to current page after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :store_location
+
+  # redirect to current page after sign in
+  def store_location
+    session[:user_return_to] = request.url unless params[:controller] == "devise/sessions"
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
 end
