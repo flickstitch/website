2dd1d5334d0593c956fbf0694d1b5bd2982b1dbc
Daniel <robotright@gmail.com>
2012-06-01 14:12:09 +0800

    Add test for voting on videos

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,9 +1,49 @@
 require 'spec_helper'
+include ControllerMacros
 
 describe VideosController do
+  let(:video) { Factory.create(:video) }
+  describe ".vote_up" do
+    # vote tests require a logged in user
+    login_user
 
-  # vote tests assume user is logged in
-  describe ".vote" do
+    before(:each) do
+      Video.stub(:find).and_return(video)
+    end
+
+    it "increase vote count by 1" do
+      expect do
+        get :vote_up, :id => video.id, :format => :json
+      end.to change{ Vote.count }.by 1
+    end
+
+    it "returns json info about vote" do
+      get :vote_up, :id => video.id, :format => :json
+      json_response = JSON.parse(response.body)
+      json_response['vote_on'].should == true
+      json_response['votes_for'].should == 1
+    end
+  end
+
+  describe ".vote_down" do
+    # vote tests require a logged in user
+    login_user
+
+    before(:each) do
+      Video.stub(:find).and_return(video)
+    end
+
+    it "vote should be false" do
+      get :vote_down, :id => video.id, :format => :json
+      Vote.first.vote.should == false
+    end
+
+    it "returns json info about vote" do
+      get :vote_down, :id => video.id, :format => :json
+      json_response = JSON.parse(response.body)
+      json_response['vote_on'].should == false
+      json_response['votes_for'].should == 0
+    end
   end
 
 end
