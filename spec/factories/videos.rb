f3c937ab434bebe5a4fb8ea84b1e0b9ddd0f3cac
Daniel <robotright@gmail.com>
2012-06-01 13:54:34 +0800

    video factory needs scene association

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,6 +3,7 @@
 FactoryGirl.define do
   factory :video do
     association(:user)
+    association(:scene)
     sequence(:name) { |n| "video#{n}" }
     video_url "http://www.youtube.com/watch?v=dQLCO9JkVeE"
   end
