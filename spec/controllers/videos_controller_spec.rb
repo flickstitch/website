3c2b3d2577744de2b31281845ba6bfa873c1e59f
Daniel <robotright@gmail.com>
2012-06-02 22:48:47 +0800

    bubble up some setup in spec

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,40 +3,43 @@ include ControllerMacros
 
 describe VideosController do
   let(:video) { Factory.create(:video) }
-  describe ".vote_up" do
-    # vote tests require a logged in user
-    login_user
 
-    before(:each) do
-      Video.stub(:find).and_return(video)
-    end
-
-    it "increase vote count by 1" do
-      expect do
-        get :vote_up, :id => video.id, :format => :json
-      end.to change{ Vote.count }.by 1
-    end
-
-    it "returns json info about vote" do
-      get :vote_up, :id => video.id, :format => :json
-      json_response = JSON.parse(response.body)
-      json_response['vote_on'].should == true
-      json_response['votes_for'].should == 1
-    end
+  before(:each) do
+    Video.stub(:find).and_return(video)
   end
 
-  describe ".vote_down" do
-    # vote tests require a logged in user
+  context "logged in user" do
     login_user
 
-    before(:each) do
-      Video.stub(:find).and_return(video)
+    describe ".vote_up" do
+      it "increase vote count by 1" do
+        expect do
+          get :vote_up, :id => video.id, :format => :json
+        end.to change{ Vote.count }.by 1
+      end
+
+      it "returns json info about vote" do
+        get :vote_up, :id => video.id, :format => :json
+        json_response = JSON.parse(response.body)
+        json_response['vote_on'].should == true
+        json_response['votes_for'].should == 1
+      end
     end
 
-    it "vote should be false" do
-      get :vote_down, :id => video.id, :format => :json
-      Vote.first.vote.should == false
+    describe ".vote_down" do
+      it "vote should be false" do
+        get :vote_down, :id => video.id, :format => :json
+        Vote.first.vote.should == false
+      end
+
+      it "returns json info about vote" do
+        get :vote_down, :id => video.id, :format => :json
+        json_response = JSON.parse(response.body)
+        json_response['vote_on'].should == false
+        json_response['votes_for'].should == 0
+      end
     end
+  end
 
     it "returns json info about vote" do
       get :vote_down, :id => video.id, :format => :json
