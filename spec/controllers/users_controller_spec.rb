88ff9ee3cb3fad942a59bc1beebd6afbad561ae0
Daniel <robotright@gmail.com>
2012-06-07 12:31:17 +0800

    add spec for verifying users cannot be deleted

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
+include ControllerMacros
 
 describe UsersController do
+
+  before(:each) do
+    User.destroy_all
+  end
+
+  login_user
+
+  it "should not be ok" do
+    { :delete => "/users" }.should_not be_routable
+  end
 
 end
